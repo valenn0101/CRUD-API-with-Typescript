b551551bd8f3d3267195a10822c13b7c5db27fa4
valenn0101 <veroncaceresvalentin@gmail.com>
2023-06-04 16:04:17 -0300

    Server on Poart and some example middleware
    Body-parser, cors and cookie-parser it's intalled

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+  type NextFunction
+} from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.middlewares();
+    // this.routes();
+  }
+
+  private config(): void {
+    // this.app.use(cors());
+    this.app.use(cookieParser());
+    this.app.use(express.json());
+    this.app.use(bodyParser.json());
+  }
+
+  private middlewares(): void {
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      res.header("Access-Control-Allow-Origin");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+      );
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie"
+      );
+      res.header("Access-Control-Allow-Credentials", "true");
+      next();
+    });
+  }
+
+  private routes(): void {
+    this.app.use("/api/v1/crud/"); // routes)
+  }
+}
+
+export default new App().app;
