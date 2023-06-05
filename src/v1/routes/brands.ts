7dd22ebc9329a3b56cd4eb73232bf6e68713467e
valenn0101 <veroncaceresvalentin@gmail.com>
2023-06-05 14:17:46 -0300

    DB and Prisma ORM
    An Example DB Get

--- src/v1/routes/brands.ts
+++ src/v1/routes/brands.ts
@@ -4,11 +4,20 @@ import {
   type Response,
   type NextFunction
 } from "express";
+import prisma from "../../config/prisma";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send({ data: "brands" });
-});
+router.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const brands = await prisma.brands.findMany();
+      res.status(200).json({ brands, message: "These are the brands" });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export { router };
